7b7aa46f537fdfd2954fc38c7c741d665b6ba212
Yucheng Chou <yuchengchou13@gmail.com>
2025-01-17 18:09:25 -0800

    updated main.py with post and get functions

---
+++ backend/main.py
@@ -0,0 +1,40 @@
+import uvicorn
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+from typing import List
+
+class Novel(BaseModel):
+    name:str
+    chapterCount:int
+
+class Novels(BaseModel):
+    novels: List[Novel]
+
+app = FastAPI()
+
+origins = [
+    "http://localhost:3000"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+memory_db = {"novels": []}
+
+@app.get(path="/novels", response_model=Novels)
+def get_novels():
+   return Novels(novels=memory_db["novels"])
+  
+
+@app.post(path="/novels", response_model=Novel)
+def add_novel(novel: Novel):
+    memory_db["novels"].append(novel)
+    return novel
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
